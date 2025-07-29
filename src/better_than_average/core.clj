f40711d111ab1e4197bb830c27b42a72836d3984
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-07-29 06:34:06 -0400

    First commit, still work to be done.

---
+++ src/better_than_average/core.clj
@@ -0,0 +1,16 @@
+(ns better-than-average.core
+  (:gen-class))
+
+;; There was a test in your class and you passed it. Congratulations!
+;; But you're an ambitious person. You want to know if you're better than the average student in your class.
+;; You receive an array with your peers' test scores. Now calculate the average and compare your score!
+;; Return true if you're better, else false!
+;; Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+(defn total_points [class_points your_points]
+  (+ your_points (reduce + class_points)))
+
+(defn )
+
+(defn better_than_average [class_points your_points]
+  )
