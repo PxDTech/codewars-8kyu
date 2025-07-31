5976b029c4bffd200cadd77d0dbaf39d44f00435
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-07-31 06:28:34 -0400

    Added calculate-average
    Write a function which calculates the average of the numbers in a given array.
    
    Note: Empty arrays should return 0.

---
+++ src/calculate_average/core.clj
@@ -0,0 +1,10 @@
+(ns calculate-average.core
+  (:gen-class))
+
+;Write a function that returns the average of a set of numbers.
+;Be sure to return longs. We don't want to return Clojure Ratios!
+
+(defn find-average
+  [numbers]
+  (/ (reduce + numbers) (count numbers))         
+  )
