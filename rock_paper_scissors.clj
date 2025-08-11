623485f73289a7a6ae62faef5231683e0ace5025
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-08-11 14:15:47 -0400

    Completed rock-paper-scissors

--- rock_paper_scissors.clj
+++ rock_paper_scissors.clj
@@ -4,4 +4,9 @@
 ;; Let's play! You have to return which player won! In case of a draw return Draw!.
 
 (defn rps [p1 p2]
-  )
+  (cond
+    (= p1 p2) "Draw!"
+    (and (= p1 "rock") (= p2 "scissors")) "Player 1 won!"
+    (and (= p1 "paper") (= p2 "rock")) "Player 1 won!"
+    (and (= p1 "scissors") (= p2 "paper")) "Player 1 won!"
+    :else "Player 2 won!"))
